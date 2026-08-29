a0b9602de78353da6cddca4a72c2710c3b8c9d69
zekeriyamm81-svg <zekeriyamm81@gmail.com>
2026-08-29 10:58:38 +0300

    Delete static directory

--- static/app.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll('input[type=file]').forEach(inp=>inp.addEventListener('change',updateFiles));
-function updateFiles(){let n=0;document.querySelectorAll('input[type=file]').forEach(i=>n+=i.files.length);const e=document.getElementById('fileSummary');if(e)e.textContent=n?`${n} dosya seçildi. Formu gönderdiğinizde fotoğraflar teklifinizle birlikte iletilecek.`:'Henüz dosya seçilmedi.'}
